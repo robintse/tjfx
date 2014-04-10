ad5b29d62c9ce98305065cfeac324ba202e5a623
robin <robin.xiep@gmail.com>
2014-04-10 21:58:00 +0800

    first commit

---
+++ app/controller/List.js
@@ -0,0 +1,15 @@
+Ext.define('MyApp.controller.List', {
+    extend: 'Ext.app.Controller',
+    
+    config: {
+        views: ['ItemTpl', 'Chart'],        refs: {            ItemTplButton: 'button[action=itemTpl]',            item: {            	autoCreate: true,	            selector: 'itemTpl',	            xtype: 'itemTpl'            }        },        
+        control: {
+            ItemTplButton: {                tap: function (list, index, target, record, e) {                	Ext.Viewport.setActiveItem(this.getItem());                }            }
+        }
+    },
+    
+    //called when the Application is launched, remove if not needed
+    launch: function(app) {
+        
+    }
+});
---
+++ app/model/ItemT.js
@@ -0,0 +1,14 @@
+Ext.define('MyApp.model.ItemT', {
+    extend: 'Ext.data.Model',
+    config: {
+        fields: [
+            { name: 'title', type: 'auto' },
+            { name: 'nszhs', type: 'auto' },
+            { name: 'zdsyh', type: 'auto' },
+            { name: 'qzdxm', type: 'auto' },
+            { name: 'szdxm', type: 'auto' },
+            { name: 'logo', type: 'auto' }
+
+        ]
+    }
+});
